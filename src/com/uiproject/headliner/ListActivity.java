e91926c16749096932e7fc654ff5fcbb33a2ee2d
zuping <lzppro@gmail.com>
2012-11-15 16:37:56 -0600

    merge viewholder and adapters into one class

--- src/com/uiproject/headliner/ListActivity.java
+++ src/com/uiproject/headliner/ListActivity.java
@@ -8,23 +8,29 @@ import java.util.Map;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 import android.view.ActionMode;
 import android.view.ContextMenu;
 import android.view.ContextMenu.ContextMenuInfo;
 import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.View.OnLongClickListener;
 import android.widget.AbsListView.MultiChoiceModeListener;
 import android.widget.AdapterView;
 import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.BaseAdapter;
+import android.widget.CheckBox;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class ListActivity extends Activity {
@@ -122,5 +128,61 @@ public class ListActivity extends Activity {
             }
         }       
     }
+	
+	private class MyAdapter extends BaseAdapter {
+		
+		private LayoutInflater inflater;
+		private List<Map<String, Object>> listItem;
+		
+		public MyAdapter(final Context context, final List<Map<String, Object>> _listItem) {
+			inflater = LayoutInflater.from(context);
+			listItem = _listItem;
+		}
+		
+		public int getCount() {
+			return listItem.size();
+		}
+
+		public View getView(int position, View convertView, ViewGroup parent) {
+			ViewHolder holder = null;
+			if(convertView == null) {
+				holder = new ViewHolder();
+				convertView = inflater.inflate(R.layout.listitem, null);
+				holder.starBox = (CheckBox) convertView.findViewById(R.id.starCheckBox);
+				holder.title = (TextView) convertView.findViewById(R.id.titleTextView);
+				holder.abstracts = (TextView) convertView.findViewById(R.id.absTextView);
+				holder.date = (TextView) convertView.findViewById(R.id.dateTextView);
+				convertView.setTag(holder);
+			} else {
+				holder = (ViewHolder) convertView.getTag();
+			}
+			HashMap<String, Object> map = (HashMap<String, Object>) listItem.get(position);
+			if( (Boolean)map.get("starBox") ) 
+				holder.starBox.setChecked(true);
+			else
+				holder.starBox.setChecked(false);
+			holder.title.setText((String)map.get("title"));
+			holder.abstracts.setText((String)map.get("abstract"));
+			holder.date.setText((String)map.get("date"));
+			return convertView;
+		}
+
+		public Object getItem(int arg0) {
+			return null;
+		}
+
+		public long getItemId(int position) {
+			return 0;
+		}
+		
+		private class ViewHolder {
+			public CheckBox starBox;
+			public TextView title;
+			public TextView abstracts;
+			public TextView date;
+			public String url;
+		}
+		
+	}
 
 }
